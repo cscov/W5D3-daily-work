05502f32edd9c1e46b0f9b94ee2b61262e923c86
cscov <carolyn.l.scoville@gmail.com>
2018-03-14 17:58:36 -0700

    Committing phases 1, 2, and broken 3

---
+++ lib/controller_base.rb
@@ -0,0 +1,55 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req, @res = req, res
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise "Already rendered or redirected" if already_built_response?
+    res.location = url
+    res.status = 302
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise "Already rendered" if already_built_response?
+    res['Content-Type'] = content_type
+    res.write(content)
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    controller_name = 'MyController'.underscore
+    template_name = File.read("views/#{controller_name}/#{template_name}.html.erb")
+    b = binding
+    formatted_template = ERB.new("<%= template_name %>").result b
+    (render_content(formatted_template, 'text/html'))
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
